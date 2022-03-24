594879a151e6663d9ddc4ef89e0a8cca9112090e
BT_1E10_17 <sample@bths.edu>
2022-03-24 13:44:57 -0400

    Initial commit

---
+++ src/Arc.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+/**
+ * Class Arc:  inherits from Shape and draws an arc
+ * @author Jacqueline Louie
+ */
+public class Arc extends Shape
+{
+    private int startAngle;
+    private int arcAngle;
+
+    public Arc()
+    {
+        super();
+        startAngle = 30;
+        arcAngle = 70;
+    }
+
+    @Override
+    public void draw(Graphics g)
+    {
+        g.setColor(getColor());
+        g.drawArc(getMinX(),getMinY(),getWidth(),getHeight(),startAngle,arcAngle);
+    }
+}
